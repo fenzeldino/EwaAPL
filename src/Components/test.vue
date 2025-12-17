19335069fde42ee567128b0aba57f1d5dad44198
fenzeldino <menzeldaniel2@gmail.com>
2025-12-17 18:00:15 +0100

    added randompic

--- src/Components/test.vue
+++ src/Components/test.vue
@@ -2,7 +2,7 @@
 import {ref,onMounted } from 'vue';
 const geschenke = ref([]);
 onMounted(() => {
-    fetch("api/ewa/demos/php_dbtab_to_json/export_json.php")
+    fetch("api/ewa/g13/aplbeleg/Weihnachtsgeschenke-Shop/src/assets/test_json.php")
             .then(res => res.json())
             .then(data =>{
             geschenke.value = data;
@@ -14,9 +14,17 @@ onMounted(() => {
         
 </script> 
 <template>
-    <div v-for="artikel in geschenke" :key="artikel.ProduktID">
-        Artikel-ID: {{ artikel.ProduktID}}
-    </div>
-     <p v-if="geschenke.length === 0">Lade Daten...</p>
+    <div v-if="geschenke.length === 0" class="loading">
+           Lade Produkte...
+       </div>
+   <figure>
+        <img src="/photos/download.jpeg"></img>
+        <figcaption>Bildinformationen</figcaption>
+   </figure>
+   
 </template>
-<style scoped></style>
+<style scoped>
+    p{
+        padding-left: 10px;
+    }
+</style>
